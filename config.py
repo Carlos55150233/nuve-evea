270c2d7febd77ec3c4cda6218b70cb27a5cb3017
Carlos55150233 <102787946+Carlos55150233@users.noreply.github.com>
2022-04-07 00:28:31 -0400

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,15 @@
-BOT_TOKEN = '5141588130:AAHRUgFLU_EcWWpYU9UlcnpGsmhGMZsSiPA'
+BOT_TOKEN = '5229426015:AAGtjazceZ_gP5OTI56kxIpKuOeI4cz30kc'
 API_ID = 18693993
 API_HASH = '382ee6b53bdd0df66a52ea9779c62424'
 
 
 # DATOS DEL MOODLE
-USUARIO = 'amaguilar'
-PASSWORD = 'Al2+1425'
-USUARIO_ID = 2374
+USUARIO = 'yquevedo@estudiantes.fbio.uh.cu'
+PASSWORD = 'lamaslinda98.Super.'
+USUARIO_ID = 1747
 ZIP_MB = 100
-MOODLE_URL = 'moodle.uclv.edu.cu'
-OWNER = [1511485191]
+MOODLE_URL = 'evea.uh.cu'
+OWNER = [998236322]
 
 # CUENTA DE MEGA
 GMAIL_MEGA = 'yanco148@gmail.com'
